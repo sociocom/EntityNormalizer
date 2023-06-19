bd8332c89e6a1b20ef49bcf3c4e7d0f3eb263232
Gabriel Andrade <gabrielhbandrade@hotmail.com>
2023-06-19 13:32:36 +0900

    Upload to PyPI

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     description='Library for normalizing entities based on a dictionary',
     author='Gabriel Herman Bernardim Andrade',
     license='MIT',
+    url='https://github.com/sociocom/EntityNormalizer',
+    download_url='https://github.com/sociocom/EntityNormalizer/archive/refs/tags/0.1.0.tar.gz',
     py_modules=['EntityNormalizer'],
     install_requires=['pandas', 'rapidfuzz', 'mojimoji'],
 )
